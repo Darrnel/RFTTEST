8b89daeb4becc1ec8ad6d79a08295d70c4234c9a
marton-szabo-96 <39117641+marton-szabo-96@users.noreply.github.com>
2018-11-07 22:42:20 +0100

    modelll created guyz

---
+++ modelll/Lyrics.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gepeles;
+
+/**
+ *
+ * @author marci
+ */
+public class Lyrics {
+    
+    private long id;
+    private String author;
+    private String album;
+    private String title;
+    private String genre;
+    private String lyrics;
+    private String uploaderID;
+    private int voteCounter;
+    
+}
---
+++ modelll/Rang.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gepeles;
+
+/**
+ *
+ * @author marci
+ */
+public enum Rang {
+    BATFAI,UJONC,KEZDO,TAPASZTALT,GOD,BALLA,JESZY;
+}
