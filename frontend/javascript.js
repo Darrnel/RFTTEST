84871b59ad4bff938851193e4a0c7c68a258e5db
Barna Vilmos <redzsiby@gmail.com>
2018-11-08 18:52:53 +0100

    login/signup betéve
    mind a 2 bugos még, javítás szükséges more

---
+++ frontend/javascript.js
@@ -0,0 +1,18 @@
+
+var modal1 = document.getElementById('registerForm');
+modal1.backdrop = "static";
+modal1.keyboard = false;
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal1) {
+        modal1.style.display = "none";
+    }
+}
+var modal2 = document.getElementById('loginForm');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+}
