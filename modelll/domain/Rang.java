db60ae92b504209c46c03db22e660a0270c7ede5
Kozma Richard <holydins10@gmail.com>
2018-11-08 20:36:49 +0100

    Create Domain package.
    A domain package tartalmazza az entitás osztályokat.

---
+++ modelll/domain/Rang.java
@@ -0,0 +1,15 @@
+package com.example.projectforrft.domain;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gepeles;
+
+/**
+ *
+ * @author marci
+ */
+public enum Rang {
+    BATFAI,UJONC,KEZDO,TAPASZTALT,GOD,BALLA,JESZY;
+}
